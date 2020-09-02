2cda2d88f18d2dd9d5e71a7d996d63470cef4d90
Yehezkiel Gunawan <yehezkiel.gunawan@student.umn.ac.id>
2020-09-02 15:27:32 +0700

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import {NgxPrinterModule} from'ngx-printer';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxPrinterModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgxPrinterService} from 'ngx-printer';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private printerService: NgxPrinterService) {}
 
+  printWindow(){
+    this.printerService.printCurrentWindow();
+  }
+  printById(){
+    this.printerService.printDiv('container');
+  }
+  printPic(){
+    this.printerService.printImg('../../assets/shapes.svg')
+  }
+
+  // printHTMLElement(){
+  //   this.printerService.printHTMLElement(<b>Test</b>);
+  // }
 }
